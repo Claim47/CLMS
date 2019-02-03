317f769c4c3ea3257b650646036177dfcaa5eb60
shoib <sagkhan@gmail.com>
2019-02-03 17:14:46 -0600

    RankCard to php

--- index.php
+++ index.php
@@ -10,13 +10,18 @@ include('template/header.php');
     
             <div class="row">
               <div class="col-md-12">
-                <div class="list-group">
-                  <a href="#" class="list-group-item list-group-item-action active"> Cras justo odio </a>
-                  <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
-                  <a href="#" class="list-group-item list-group-item-action">aaaaMorbi leo risus</a>
-                  <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-                  <a href="#" class="list-group-item list-group-item-action disabled">Vestibulum at eros</a>
+                <div class="row">
+                  <div class="col-md-4">
+                  <?php include('template/includes/RankCard.php'); ?>
+                  </div>
+                  <div class="col-md-4">
+                  <?php include('template/includes/RankCard.php'); ?>
+                  </div>
+                  <div class="col-md-4">
+                  <?php include('template/includes/RankCard.php'); ?>
+                  </div>
                 </div>
+      
               </div>
             </div>
     </div>
---
+++ template/includes/RankCard.php
@@ -0,0 +1,41 @@
+<div class="twPc-div ">
+                            <span class="twPc-bg twPc-block twPc-title ">Premier Batting</span>
+
+                            <div>
+                                <a title="Mert S. Kaplan" href="#" class="twPc-avatarLink">
+                                    <img alt="Mert S. Kaplan" src="assets/icons/profile.jpg" class="twPc-avatarImg">
+                                </a>
+
+                                <div class="twPc-divUser">
+                                    <div class="twPc-divName">
+                                        <a href="#">Mert S. Kaplan</a>
+                                    </div>
+                                    <span>
+                                        <a href="#">@<span>mertskaplan</span></a>
+                                    </span>
+                                </div>
+
+                                <div class="twPc-divStats">
+                                        <ul class="twPc-Arrange">
+                                            <li class="twPc-ArrangeSizeFit bg-primary text-white">
+                                                <span href="#" title="9.840 Tweet">
+                                                    <span class="twPc-StatLabel twPc-block ">Runs</span>
+                                                    <span class="twPc-StatValue">240</span>
+                                                </span>
+                                            </li>
+                                            <li class="twPc-ArrangeSizeFit bg-warning text-dark">
+                                                <span href="#" title="885 Following">
+                                                    <span class="twPc-StatLabel twPc-block">Innings</span>
+                                                    <span class="twPc-StatValue">6</span>
+                                                </span>
+                                            </li>
+                                            <li class="twPc-ArrangeSizeFit bg-info text-white">
+                                                <span href="#" title="1.810 Followers">
+                                                    <span class="twPc-StatLabel twPc-block">Avg</span>
+                                                    <span class="twPc-StatValue">1.810</span>
+                                                </span>
+                                            </li>
+                                        </ul>
+                                    </div>
+                            </div>
+                        </div>
