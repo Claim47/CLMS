b69d88a809acba53563ed27a3173140d643842ed
shoib <sagkhan@gmail.com>
2019-02-05 00:43:18 -0600

    LiveGame

--- liveGames.php
+++ liveGames.php
@@ -8,7 +8,71 @@ include('template/header.php');
   <div class="row" >
     <div class="col-md-12">
     
-    
+    <div class="col-md-4 games-schedule">
+                            <div class="well">
+                                <div class="games-schedule-title">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <h5>Todays Games</h5>
+                                        </div>
+                                    </div>
+                                </div>                
+                                <div class="games-schedule-items">
+                                    <div class="row games-team">
+                                        <div class="col-md-5 games-teamlogo">
+                                            <img src="assets/teams/SampleTeam1.png" alt="Bangladesh">
+                                            <span>Bangladesh</span>
+                                        </div>
+                                        <div class="col-md-2">
+                                            <h4 class="img-circle">VS</h4>
+                                        </div>
+                                        <div class="col-md-5 games-teamlogo">
+                                            <img src="assets/teams/SampleTeam2.png" alt="Australia">
+                                            <span>Australia</span>
+                                        </div>
+                                    </div>
+                                    <div class="row games-info">
+                                        <div class="col-md-12">
+                                            <p><span class="glyphicon glyphicon-play-circle"></span> 19 March, 2014 (<small>15:30 local | 09:30 GMT</small>)</p>
+                                            <p class="games-dash"></p>
+                                            <p><small>Mirpur Internation Stadium, Dhaka</small></p>
+                                        </div>
+                                    </div>
+                                </div>  
+                                <div class="games-schedule-items">
+                                    <div class="row games-team">
+                                        <div class="col-md-5">
+                                            <img src="http://placehold.it/115x67" alt="Pakistan">
+                                            <span>Pakistan</span>
+                                        </div>
+                                        <div class="col-md-2">
+                                            <h4 class="img-circle">VS</h4>
+                                        </div>
+                                        <div class="col-md-5">
+                                            <img src="http://placehold.it/115x67" alt="India">
+                                            <span>India</span>
+                                        </div>
+                                    </div>
+                                    <div class="row games-info">
+                                        <div class="col-md-12">
+                                            <p><span class="glyphicon glyphicon-play-circle"></span> 19 March, 2014 (<small>19:30 local | 13:30 GMT</small>)</p>
+                                            <p class="games-dash"></p>
+                                            <p><small>Fahtullah Internation Stadium, Narayanganj</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="games-schedule-footer">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <p class="text-left">19 March, 2014</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p class="text-right">&COPY; bootstrap.com</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
       
   </div>
 </div>
